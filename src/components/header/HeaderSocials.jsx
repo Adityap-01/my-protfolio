7ade50dda117ec1f047ca36bf21448a9b2ccbf63
Aditya <adityapradhan11@gmail.com>
2022-09-12 23:09:32 +0530

    Header Done

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {BsInstagram} from 'react-icons/bs'
+import  LOGO from '../../assests/LOGO.png'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        
+        <div className="logo">
+        <img src={LOGO} alt="logo" />
+      </div>
+        <a href="https://www.linkedin.com/in/aditya-pradhan-15964a1b2/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/Adityap-01" target="_blank"><FaGithub/></a>
+        <a href="https://www.instagram.com/adityapradhan_01/" target="_blank"><BsInstagram/></a>
+    </div>
+  )
+}
+
+export default HeaderSocials

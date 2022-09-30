74587b31e1540e10609d7dcae1d8648488ecdd0c
Aditya <adityapradhan11@gmail.com>
2022-10-01 01:56:41 +0530

    footer done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,26 @@ import './contact.css'
 import { MdOutlineEmail } from 'react-icons/md'
 import { RiMessengerLine } from 'react-icons/ri'
 import { BsWhatsapp } from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_jliml8b', 'template_lxxcf1k', form.current, 'zepIgsjFcL6lriraR')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+    e.target.reset()
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -32,7 +51,7 @@ const Contact = () => {
             <a href="https://api.whatsapp.com/send?phone=+919009480834" target="_blank">Send a message</a>
           </article>
         </div>
-        <form actions="">
+        <form ref={form} onSubmit={sendEmail}>
             <input type="text" name="name" placeholder='Your Full Name' required />
             <input types="email" name="email" placeholder='Your Email' required />
             <textarea name="message" rows="7" placeholder ='your Message' required> </textarea>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+import {BsFacebook} from 'react-icons/bs'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Aditya</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/aditya.pradhan.754/"><BsFacebook/></a>
+        <a href="https://www.instagram.com/adityapradhan_01/"><BsInstagram/></a>
+        <a href="https://www.linkedin.com/in/aditya-pradhan-15964a1b2/"><BsLinkedin/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Aditya Pradhan. All rights reserved. </small>
+      </div>
+    </footer>
   )
 }
 
